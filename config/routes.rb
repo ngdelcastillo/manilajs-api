89ae2e87a3be9b4791845f6f22617bfe66f65d56
Noel del Castillo <ngdelcastillo@gmail.com>
2015-07-08 22:57:02 +0800

    Adds home and ActionDispatch::Flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users, controllers: { sessions: 'sessions' }
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
